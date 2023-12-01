243fae668cc1d4ba9f0e6a7ee4b854ba6f9a1cfd
Taylor Bisset <taylorbisset@gmail.com>
2023-11-30 16:22:59 -0800

    Copied over main.

--- CS132-OP1-MergeForLinkedList/main.cpp
+++ CS132-OP1-MergeForLinkedList/main.cpp
@@ -12,10 +12,134 @@ Repo:           https://github.com/TaylorBisset/CS132-OP1-MergeForLinkedList
 */
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "LkList.h"
+#include "stdafx.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	LkList list, list2;
+
+	//
+	//list.insert( { 0, 10, 20, 30, 40} );
+	//list2.insert( { 1, 2, 3, 5, 11, 12, 13, 26, 27, 28, 29, 34, 35, 36, 37, 44, 45, 46, 47} );
+	//
+	//list.merge(list2);
+	//
+	//cout << "Merge #1 (source overlaps destination): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+
+	//list.merge(list2);
+	//
+	//cout << "Merge #2 (empty merge): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+	//list2.insert( { 0, 10, 20, 30, 40 } );
+
+	//list.merge(list2);
+
+	//cout << "Merge #3 (into an empty list): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+	//
+	//list.insert( { 0, 5, 10, 15, 20, 25, 30, 35, 40} );
+	//
+	//
+	//list.merge(list2);
+	//
+	//cout << "Merge #4 (from an empty list): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+
+	//list.insert( { 40, 47, 54, 61, 68 } );
+	//
+	//list2.insert( { 0, 3, 6, 9, 12, 15, 18 } );
+
+
+	//list.merge(list2);
+
+	//cout << "Merge #5(source before destination): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+	//
+	//------------------------
+
+	//list.insert( { 10, 17, 21, 28 } );
+	//
+	//list2.insert( { 50, 53, 56, 59, 62, 65 } );	
+	//
+	//list.merge(list2);
+	//
+	//cout << "Merge #6(source after destination): \n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+	//list.insert( { 0, 9, 18, 27 } );
+	//
+	//list2.insert( { -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 16, 
+	//                 18, 20, 22, 24, 26, 28, 30, 32, 34 } );	
+	//
+	//list.merge( list2 );
+	//
+	//cout << "Merge #7(source overlaps destination):\n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+	//------------------------
+
+	//list.insert( { -6, 2, 10, 18, 26, 34 } );
+	//
+	//list2.insert( { 10, 13, 16, 19, 22, 25 } );	
+	//
+	//list.merge( list2 );
+	//
+	//cout << "Merge #8(source within destination):\n" << list << endl;
+	//cout << "List size after merge: " << list.size() << endl;
+	//cout << endl << endl;
+	//
+	//list.clear();
+	//list2.clear();
+
+
+	//------------------------
+
+
+	system("pause");
+	return 0;
 }
 
 /*
@@ -23,7 +147,7 @@ int main()
 For this program you will be writing and testing a merge function. 
 This function will be like the merge function that belongs to List in the STL, 
  in that it will only work if you are working with list that are already sorted. 
-If you were to use the function like this: list1.merge( list2 ); 
+If you were to use the function like this: `list1.merge( list2 );` 
  then all of the data from list2 would be moved into list1 in the correct places. 
 After the merge is completed list2 should be an empty List that can be used again 
  (in the main, I call the clear function for list2, but this should not be needed).
